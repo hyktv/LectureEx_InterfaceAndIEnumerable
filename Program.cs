3e61ed111ac746013a944156683a4e0b6a0b00b1
hyktv <hafsa.yktv@gmail.com>
2024-02-23 23:48:34 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LectureEx_InterfaceAndIEnumerable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
